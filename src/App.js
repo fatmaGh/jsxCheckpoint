a4e29486342a65af608db6f03e4709cf9fe54506
fatma <fatma.ghaarsallah@gmail.com>
2021-05-10 13:31:11 +0100

    jsx checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import image from './imageInSrc.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border: 'solid 1px black',maxWidth: '100vw'}}>
+        <div className="header">
+          <h1 className="title red">Fatma Gharsallah</h1>
+        </div>
+
+        <br />
+
+        <img src="../imageInPublic.jpg" width="300" alt="grafitti1" />
+
+        <br />
+
+        <img src={image} alt="grafitti2"  width="300"/>
+
+      </div>
+
+        <video width="100%" height="240" controls>
+
+          <source src="myVideo.mp4" type="video/mp4" />
+
+        </video>
     </div>
   );
 }
